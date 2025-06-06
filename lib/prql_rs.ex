1072cbc8dda5dbfe880b35368d27c39388a07ba1
Daniel Kukula <daniel.kuku@gmail.com>
2025-06-06 15:27:17 +0200

    initial commit

---
+++ lib/prql_rs.ex
@@ -0,0 +1,18 @@
+defmodule PrqlRs do
+  @moduledoc """
+  Documentation for `PrqlRs`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PrqlRs.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/prql_rs_test.exs
@@ -0,0 +1,8 @@
+defmodule PrqlRsTest do
+  use ExUnit.Case
+  doctest PrqlRs
+
+  test "greets the world" do
+    assert PrqlRs.hello() == :world
+  end
+end
